b462ee6b013625060f396eb9486c5fd48a40a1bf
koho <ihopeme.hd@gmail.com>
2018-01-20 15:46:46 +0800

    优化Windows投屏窗口化截图操作

--- img_utils.py
+++ img_utils.py
@@ -7,7 +7,6 @@ import config
 from PIL import Image
 import os
 import sys
-import subprocess
 
 client = AipOcr(config.APP_ID, config.API_KEY, config.SECRET_KEY)
 
@@ -28,19 +27,11 @@ def get_ios_img():
 
 
 # 投影到桌面进行截图
-def get_pc_img(window_name, box):
-    if window_name:
+def get_pc_img(window_cap, box):
+    if window_cap:
         assert sys.platform == 'win32', 'Platform is not Windows'
-        command = ['windowcap.exe', window_name.encode(sys.stdout.encoding)]
-        if not config.PC_WINDOW_FALLBACK:
-            command.append(config.IMAGE_PAGE)
-        output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
-        if output:
-            if output.startswith('('):
-                img = ImageGrab.grab(eval(output))
-                img.save(config.IMAGE_PAGE)
-            else:
-                raise ValueError(output)
+        if os.system(' '.join(('windowcap.exe', config.PC_WINDOW_CONFIG, config.IMAGE_PAGE))) != 0:
+            raise ValueError('Cannot find target window')
         crop(config.IMAGE_PAGE, get_box_by_image(config.IMAGE_PAGE, config.GET_FACTOR))
     else:
         img = ImageGrab.grab(box)
@@ -102,7 +93,7 @@ def get_file_content(filePath):
 # 识别文字
 def spot():
     if config.GET_DEVICE_TYPE == config.TYPE_PC:
-        get_pc_img(config.PC_WINDOW_NAME, config.PC_CROP_BOX)
+        get_pc_img(config.PC_USE_WINDOW_CAPTURE, config.PC_CROP_BOX)
     else:
         if config.GET_DEVICE_TYPE == config.TYPE_ANDROID:
             get_android_img()
